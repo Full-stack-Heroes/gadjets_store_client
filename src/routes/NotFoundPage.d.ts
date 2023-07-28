799b077aac25c92de92ed3d46bd649aa43ffb49c
Denys Didenko <ddidenko441@gmail.com>
2023-07-28 21:33:09 +0300

    add husky

--- src/routes/NotFoundPage.d.ts
+++ src/routes/NotFoundPage.d.ts
@@ -1,6 +1,6 @@
 import React from 'react';
 
 declare module './routes/NotFoundPage.jsx' {
-  const Root: React.ComponentType<any>;
+  const Root: React.ComponentType<unknown>;
   export default Root;
 }
