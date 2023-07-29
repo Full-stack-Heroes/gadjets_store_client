f5d1a65970e5346380626878e5fc06de618d51be
Ivan Khoroshylov <host513@gmail.com>
2023-07-29 22:16:41 +0300

    initial pagination

--- src/modules/Phones/components/PhonesContent/PhonesContent.tsx
+++ src/modules/Phones/components/PhonesContent/PhonesContent.tsx
@@ -1,10 +1,12 @@
 import { FC } from 'react';
 import styles from './PhonesContent.module.scss';
+import { Pagination } from '../../../../components/Pagination/Pagination';
 
 export const PhonesContent: FC = () => {
   return (
     <section className={styles.content}>
       <h2>I AM PHONES CONTENT!</h2>
+      <Pagination />
     </section>
   );
 };
