282bfde79933212185d01d18ba39af58359e1382
Eugene Sherepa <evgenijserepa@gmail.com>
2023-07-29 13:01:18 +0300

    add footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,14 +1,35 @@
 import { FC } from 'react';
 import styles from './Footer.module.scss';
+import logo from '../../assets/icons/Logo.svg'
+import Up from '../../assets/icons/Arrow.svg'
 
 export const Footer: FC = () => {
   return (
     <footer className={styles.footer}>
-      <div className="container">
-        <div className="footer__row">
-          <h2>THIS IS FOOTER</h2>
+       <div className={styles.footer__container}>
+        <div className={styles.footer__container__logo}>
+          <img src={logo} alt="nice gadgets" />
         </div>
-      </div>
+
+        <div className={styles.footer__container__links}>
+          <a href="https://github.com/fe-apr23-full-stack-heroes" className={styles.link}>
+            github
+          </a>
+          <a href="#contacts" className={styles.link}>
+            contacts
+          </a>
+          <a href="#rights" className={styles.link}>
+            rights
+          </a>
+        </div>
+
+        <div className={styles.footer__container__up}>
+          <span className={styles.up__text}>Back to top</span>
+          <button className={styles.button__up}>
+            <img src={Up} alt="go up button" />
+          </button>
+        </div>
+       </div>
     </footer>
   );
 };
