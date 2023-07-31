c67e38dca9f87bc5d0f08fed61b52ae1a5af2137
Eugene Sherepa <evgenijserepa@gmail.com>
2023-07-31 20:45:49 +0300

    add cart initial style

--- src/pages/TabletsPage/TabletsPage.tsx
+++ src/pages/TabletsPage/TabletsPage.tsx
@@ -1,9 +1,13 @@
 import { FC } from 'react';
+import { CartItem } from '../../components/CartItem/CartItem';
+import { TotalCost } from '../../components/TotalCost/TotalCost';
 
 export const TabletsPage: FC = () => {
   return (
     <div>
       <p>Tablets</p>
+      <CartItem />
+      <TotalCost />
     </div>
   );
 };
