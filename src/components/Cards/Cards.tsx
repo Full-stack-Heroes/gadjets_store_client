46a26a1bc0984dabb2aad0bcd7863e5c417009db
Denys Didenko <ddidenko441@gmail.com>
2023-07-31 23:15:07 +0300

    load phones from api

--- src/components/Cards/Cards.tsx
+++ src/components/Cards/Cards.tsx
@@ -1,22 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Card } from '../Card/Card';
-import styles from './cards.module.scss';
+import './cards.scss';
+import { Phone } from '../../types/phone';
+import { getPhones } from '../../utils/helpers';
 
 export const Cards: React.FC = () => {
+  const [phones, setPhones] = useState<Phone[]>([]);
+
+  useEffect(() => {
+    try {
+      const fetchData = async () => {
+        const fetchedPhones = await getPhones();
+
+        setPhones(fetchedPhones);
+      };
+
+      fetchData();
+    } catch {
+      console.log('Error');
+    }
+  }, []);
+
   return (
-    <div className={styles.cards__container}>
-      <Card className={styles.card} />
-      <Card className={styles.card} />
-      <Card className={styles.card} />
-      <Card className={styles.card} />
-      <Card className={styles.card} />
-      <Card className={styles.card} />
-      <Card className={styles.card} />
-      <Card className={styles.card} />
-      <Card className={styles.card} />
-      <Card className={styles.card} />
-      <Card className={styles.card} />
-      <Card className={styles.card} />
+    <div className="cards__container">
+      {phones.slice(3, 19).map((phone) => (
+        <Card phone={phone} key={phone.id} />
+      ))}
     </div>
   );
 };
---
+++ src/utils/helpers.ts
@@ -0,0 +1,23 @@
+import { getProducts } from '../api/phones';
+
+export const getPhones = async () => {
+  const products = await getProducts();
+
+  const phones = products.filter((product) => (
+    product.category === 'phones'
+  ));
+
+  return phones;
+};
+
+export const normalizeImage = (img: string) => {
+  const imgLink = img.slice(0, -4);
+
+  return 'https://gadjets-store-apu.onrender.com/' + imgLink + 'jpg';
+};
+
+export const normalizeMemory = (memory: string) => {
+  const capacity = memory.slice(0, -2);
+
+  return capacity + ' GB';
+};
