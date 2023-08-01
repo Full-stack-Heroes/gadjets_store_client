bafaafed18734e304a16ff3ddfd3def2abe3f8a6
Ivan Khoroshylov <host513@gmail.com>
2023-08-01 22:22:49 +0300

    add component backlink

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -7,40 +7,37 @@ import { scrollToTop } from '../../utils/helpers';
 
 export const Footer: FC = () => {
   return (
-    <div className={styles.wrapper}>
-      <footer className={styles.footer}>
-        <div className={styles.footer__container}>
-          <div className={styles.footer__container__logo}>
-            <Link to="/">
-              <img src={logo} alt="nice gadgets" />
-            </Link>
-          </div>
-
-          <div className={styles.footer__container__links}>
-            <Link
-              to="https://github.com/fe-apr23-full-stack-heroes"
-              className={styles.link}>
-            github
-            </Link>
-            <Link to="/contacts" className={styles.link}>
-            contacts
-            </Link>
-            <Link to="/rights" className={styles.link}>
-            rights
-            </Link>
-          </div>
-
-          <div
-            className={styles.footer__container__up}
-            onClick={() => scrollToTop()}>
-            <span className={styles.up__text}>Back to top</span>
-            <button className={styles.button__up}>
-              <img src={Up} alt="go up button" />
-            </button>
-          </div>
+    <footer className={styles.footer}>
+      <div className={styles.footer__container}>
+        <div className={styles.footer__container__logo}>
+          <Link to="/">
+            <img src={logo} alt="nice gadgets" />
+          </Link>
         </div>
-      </footer>
-    </div>
 
+        <div className={styles.footer__container__links}>
+          <Link
+            to="https://github.com/fe-apr23-full-stack-heroes"
+            className={styles.link}>
+              github
+          </Link>
+          <Link to="/contacts" className={styles.link}>
+              contacts
+          </Link>
+          <Link to="/rights" className={styles.link}>
+              rights
+          </Link>
+        </div>
+
+        <div
+          className={styles.footer__container__up}
+          onClick={() => scrollToTop()}>
+          <span className={styles.up__text}>Back to top</span>
+          <button className={styles.button__up}>
+            <img src={Up} alt="go up button" />
+          </button>
+        </div>
+      </div>
+    </footer>
   );
 };
