002bd8bae3813e1b69c11239d8bba6c3869fd589
Eugene Sherepa <evgenijserepa@gmail.com>
2023-08-01 10:34:16 +0300

    rework button logic on checkout

--- src/components/TotalCost/TotalCost.tsx
+++ src/components/TotalCost/TotalCost.tsx
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import cn from 'classnames';
 import styles from './TotalCost.module.scss';
 
 export const TotalCost: React.FC = () => {
+  const [isCheckoutDone, setCheckoutDone] = useState(false);
+  const buttonTitle = isCheckoutDone ? 'Thanks for shopping' : 'Checkout';
+
+  const handleCheckout = () => {
+    if (isCheckoutDone) {
+      window.location.href = '/';
+    } else {
+      setCheckoutDone(true);
+    }
+  };
+
   return (
     <div className={styles.total__container}>
       <div className={styles.total__container_cost}>$2657</div>
 
       <div className={styles.total__container_items}>Total for X items</div>
 
-      <button className={styles.total__container_button}>Checkout</button>
+      <button
+        className={cn (styles.total__container_button, {
+          [styles.total__container_button_active]: isCheckoutDone,
+        })}
+        onClick={() => handleCheckout()}
+      >
+        {buttonTitle}
+      </button>
     </div>
   );
 };
