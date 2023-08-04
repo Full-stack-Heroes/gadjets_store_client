5db63cd7ef199333ee0ad048271522db690fd722
Denys Didenko <ddidenko441@gmail.com>
2023-08-04 20:52:31 +0300

    change product page by color and capacity

---
+++ src/components/styles/productColors.ts
@@ -0,0 +1,23 @@
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const productColors: any = {
+  starlight: '#2e2e2e',
+  midnight: '#191970',
+  purple: '#800080',
+  blue: '#0000ff',
+  green: '#008000',
+  rosegold: '#b76e79',
+  spaceblack: '#222222',
+  black: '#000000',
+  pink: '#ffc0cb',
+  red: '#ff0000',
+  spacegray: '#808080',
+  silver: '#c0c0c0',
+  midnightgreen: '#004953',
+  skyblue: '#87ceeb',
+  graphite: '#5F7170',
+  coral: '#ff7f50',
+  gold: '#FCDBC1',
+  sierrablue: '#005b9a',
+  yellow: '#ffff00',
+  white: '#f0f0e0'
+};
