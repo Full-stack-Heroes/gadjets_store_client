05806bb2da94386192e8a62d6e2b236b4af503bd
Victor P <vpanya97@gmail.com>
2023-08-06 02:19:20 +0300

    add promo slider to home page.Fix grids in welcom

--- src/modules/Home/Home.tsx
+++ src/modules/Home/Home.tsx
@@ -1,13 +1,13 @@
 import { FC } from 'react';
 import { HomeLayout } from './components/HomeLayout';
 import { WelcomeTitle } from './components/WelcomeTitle';
-// import { PromoSlider } from './components/PromoSlider';
+import { PromoSlider } from './components/PromoSlider';
 
 export const Home: FC = () => {
   return (
     <HomeLayout>
       <WelcomeTitle />
-    //   <PromoSlider />
+      <PromoSlider />
     </HomeLayout>
   );
 };
