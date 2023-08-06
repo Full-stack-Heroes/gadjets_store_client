99454c7bacf98cd07958b8d9696de11934070fcb
Victor P <vpanya97@gmail.com>
2023-08-06 11:28:44 +0300

    fix imports

--- src/modules/Home/Home.tsx
+++ src/modules/Home/Home.tsx
@@ -2,12 +2,14 @@ import { FC } from 'react';
 import { HomeLayout } from './components/HomeLayout';
 import { WelcomeTitle } from './components/WelcomeTitle';
 import { PromoSlider } from './components/PromoSlider';
+import { HomeContent } from './components/HomeContent';
 
 export const Home: FC = () => {
   return (
     <HomeLayout>
       <WelcomeTitle />
       <PromoSlider />
+      <HomeContent />
     </HomeLayout>
   );
 };
