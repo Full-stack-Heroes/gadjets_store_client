7689582d02b6120a091a2588cfc0c1d867e7cb75
Victor P <vpanya97@gmail.com>
2023-08-08 17:47:43 +0300

    add burger animation and optimize its behavior

--- src/components/BurgerMenuOpened/BurgerMenuOpened.tsx
+++ src/components/BurgerMenuOpened/BurgerMenuOpened.tsx
@@ -1,64 +1,88 @@
-import { FC, useState } from 'react';
+import { FC } from 'react';
 import like from '../../assets/icons/Heart.svg';
 import cart from '../../assets/icons/Cart.svg';
 import styles from './BurgerMenuOpened.module.scss';
-import { BurgerMenuProps } from '../../types/BurgerMenuProps';
+import { Link } from 'react-router-dom';
 import { NavigationLink } from '../NavigationLink';
 import classNames from 'classnames/bind';
+import { HeaderCounter } from '../../HeaderCounter/HeaderCounter';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../store';
+import { Product } from '../../types/product';
 
 const cn = classNames.bind(styles);
 
-export const BurgerMenuOpened: FC<BurgerMenuProps> = ({ isMenuOpen, setIsMenuOpen }) => {
-  const [isCartActive, setIsCartActive] = useState(false);
-  const [isLikeActive, setIsLikeActive] = useState(false);
+interface BurgerMenuProps {
+  isMenuOpen: boolean;
+  setIsMenuOpen: (isOpen: boolean) => void;
+}
 
-  const handleServiceButtonClick = (buttonType: 'cart' | 'like') => {
-    setIsCartActive(buttonType === 'cart');
-    setIsLikeActive(buttonType === 'like');
+export const BurgerMenuOpened: FC<BurgerMenuProps> = ({
+  isMenuOpen,
+  setIsMenuOpen,
+}) => {
+
+  const handleMenuClose = () => {
     setIsMenuOpen(false);
   };
 
+  const products = useSelector(
+    (state: RootState) => state.cart.cartItems as Product[],
+  );
+
+  const likedProducts = useSelector(
+    (state: RootState) => state.favorites.favoriteItems as Product[],
+  );
+
   return (
-    <div className={cn(isMenuOpen ? 'burger' : '')}>
+    <div className={cn('burger', { 'burger_opened': isMenuOpen })}>
       <div className={cn('burger__links')}>
-        <nav className={cn(isMenuOpen ? 'burger__nav' : '')}>
-          <NavigationLink to="/" linkText="Home" onClick={() => setIsMenuOpen(false)} />
-          <NavigationLink to="/phones" linkText="Phones" onClick={() => setIsMenuOpen(false)} />
-          <NavigationLink to="/tablets" linkText="Tablets" onClick={() => setIsMenuOpen(false)} />
-          <NavigationLink to="/accessories" linkText="Accessories" onClick={() => setIsMenuOpen(false)} />
+        <nav className={cn('burger__nav')}>
+          <NavigationLink
+            to="/"
+            linkText="Home"
+            onClick={handleMenuClose}
+          />
+          <NavigationLink
+            to="/phones"
+            linkText="Phones"
+            onClick={handleMenuClose}
+          />
+          <NavigationLink
+            to="/tablets"
+            linkText="Tablets"
+            onClick={handleMenuClose}
+          />
+          <NavigationLink
+            to="/accessories"
+            linkText="Accessories"
+            onClick={handleMenuClose}
+          />
         </nav>
       </div>
 
-      <div className={cn(isMenuOpen ? 'burger__service' : '')}>
-        <button
-          className={cn(
-            'service__button',
-            'service__like',
-            { 'service__button-active': isLikeActive }
-          )}
-          onClick={() => handleServiceButtonClick('like')}
+      <div className={cn('burger__service')}>
+        <Link
+          to="/favourites"
+          className={cn('service__button', 'service__like', {
+            'service__button-active': isMenuOpen,
+          })}
+          onClick={handleMenuClose}
         >
-          <img
-            src={like}
-            alt="like button"
-            className={cn('like')}
-          />
-        </button>
+          <img src={like} alt="like button" className={cn('like')} />
+          <HeaderCounter products={likedProducts}/>
+        </Link>
 
-        <button
-          className={cn(
-            'service__button',
-            'service__cart',
-            { 'service__button-active': isCartActive }
-          )}
-          onClick={() => handleServiceButtonClick('cart')}
+        <Link
+          to="/cart"
+          className={cn('service__button', 'service__cart', {
+            'service__button-active': isMenuOpen,
+          })}
+          onClick={handleMenuClose}
         >
-          <img
-            src={cart}
-            alt="cart button"
-            className={cn('cart')}
-          />
-        </button>
+          <img src={cart} alt="cart button" className={cn('cart')} />
+          <HeaderCounter products={products}/>
+        </Link>
       </div>
     </div>
   );
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,32 +3,31 @@ import styles from './Header.module.scss';
 import logo from '../../assets/images/Logo.svg';
 import like from '../../assets/icons/Heart.svg';
 import cart from '../../assets/icons/Cart.svg';
+import menu from '../../assets/icons/Menu.svg';
+import close from '../../assets/icons/Close.svg';
+import { NavLink } from 'react-router-dom';
 import { NavigationLink } from '../NavigationLink';
-import { BurgerMenuButton } from '../BurgerMenuButton';
 import classNames from 'classnames/bind';
 import { BurgerMenuOpened } from '../BurgerMenuOpened';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../store';
+import { Product } from '../../types/product';
+import { HeaderCounter } from '../../HeaderCounter/HeaderCounter';
 
 const cn = classNames.bind(styles);
 
 export const Header: FC = () => {
+  const products = useSelector(
+    (state: RootState) => state.cart.cartItems as Product[],
+  );
+
+  const likedProducts = useSelector(
+    (state: RootState) => state.favorites.favoriteItems as Product[],
+  );
+
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
-  // Opened burger and increasing width 640+ not hidding header elems
-  const handleWindowResize = () => {
-    if (window.innerWidth > 640 && isMenuOpen) {
-      setIsMenuOpen(false);
-    }
-  };
-
-  useEffect(() => {
-    window.addEventListener('resize', handleWindowResize);
-
-    return () => {
-      window.removeEventListener('resize', handleWindowResize);
-    };
-  }, [isMenuOpen]);
-
-  // Remove scrollbar when burger opened.
   useEffect(() => {
     if (isMenuOpen) {
       document.body.style.overflow = 'hidden';
@@ -37,51 +36,102 @@ export const Header: FC = () => {
     }
   }, [isMenuOpen]);
 
+  const handleMenuClick = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const handleResize = () => {
+    setWindowWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (windowWidth > 640 && isMenuOpen) {
+      setIsMenuOpen(false);
+    }
+  }, [windowWidth, isMenuOpen]);
+
   return (
     <header className={cn('header')}>
       <div className={cn('container')}>
         <div className={cn('links')}>
-          <img
-            src={logo}
-            alt="Nice Gadgets logo"
-            className={cn('header__logo')}
-          />
+          <NavLink to="/">
+            <img
+              src={logo}
+              alt="Nice Gadgets logo"
+              className={cn('header__logo')}
+            />
+          </NavLink>
 
-          {!isMenuOpen && <nav className={cn('nav')}>
-            <NavigationLink to="/" linkText="Home" />
-            <NavigationLink to="/phones" linkText="Phones" />
-            <NavigationLink to="/tablets" linkText="Tablets" />
-            <NavigationLink to="/accessories" linkText="Accessories" />
-          </nav>}
+          {!isMenuOpen && (
+            <nav className={cn('nav')}>
+              <NavigationLink to="/" linkText="Home" />
+              <NavigationLink to="/phones" linkText="Phones" />
+              <NavigationLink to="/tablets" linkText="Tablets" />
+              <NavigationLink to="/accessories" linkText="Accessories" />
+            </nav>
+          )}
         </div>
 
         <div className={cn('header__service')}>
-          <button className={cn('service_btn')}>
+          <NavLink
+            to="/favourites"
+            className={({ isActive }) =>
+              cn('service_btn', {
+                active: isActive,
+              })
+            }>
             <img
               src={like}
               alt="like button"
               className={cn('service_btn_img')}
             />
-          </button>
 
-          <button className={cn('service_btn')}>
+            <div className={cn('headerCounter')}>
+              <HeaderCounter products={ likedProducts }/>
+            </div>
+          </NavLink>
+
+          <NavLink
+            to="/cart"
+            className={({ isActive }) =>
+              cn('service_btn', {
+                active: isActive,
+              })
+            }>
             <img
               src={cart}
               alt="like button"
               className={cn('service_btn_img')}
             />
-          </button>
+            <div className={cn('headerCounter')}>
+              <HeaderCounter products={ products }/>
+            </div>
+          </NavLink>
         </div>
 
-        <BurgerMenuButton isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen}/>
+        <button
+          className={cn('burger_btn', { 'burger_btn-active': isMenuOpen })}
+          onClick={handleMenuClick}>
+          <img
+            src={isMenuOpen ? close : menu}
+            alt={isMenuOpen ? 'close button' : 'menu button'}
+            className={cn('burger_btn_img')}
+          />
+        </button>
       </div>
 
-      {isMenuOpen && (
-        <BurgerMenuOpened
-          isMenuOpen={isMenuOpen}
-          setIsMenuOpen={setIsMenuOpen}
-        />
-      )}
+      <BurgerMenuOpened
+        isMenuOpen={isMenuOpen}
+        setIsMenuOpen={setIsMenuOpen}
+      />
     </header>
   );
 };
