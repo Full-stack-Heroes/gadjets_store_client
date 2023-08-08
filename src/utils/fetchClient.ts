fe4281d8bf5996b6b6a9730211eb43d03ee885f8
Ivan Khoroshylov <host513@gmail.com>
2023-08-08 08:54:30 +0300

    add cart link on item, fix footer

--- src/utils/fetchClient.ts
+++ src/utils/fetchClient.ts
@@ -1,4 +1,5 @@
-export const BASE_URL = 'https://gadjets-store-apu.onrender.com';
+// export const BASE_URL = 'https://gadjets-store-apu.onrender.com';
+export const BASE_URL = 'http://localhost/:3000';
 
 function wait(delay: number) {
   return new Promise((resolve) => {
