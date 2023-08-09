59e595acfba1a100a1c8d3465f8f2dcbfe3c2973
Pavlo Levchenko <78237301+hekaq@users.noreply.github.com>
2023-08-09 12:06:08 +0200

    Update fetchClient.ts

--- src/utils/fetchClient.ts
+++ src/utils/fetchClient.ts
@@ -1,5 +1,4 @@
-// export const BASE_URL = 'https://gadjets-store-apu.onrender.com';
-export const BASE_URL = 'http://localhost:3000';
+export const BASE_URL = 'https://gadjets-store-apu.onrender.com';
 
 function wait(delay: number) {
   return new Promise((resolve) => {
