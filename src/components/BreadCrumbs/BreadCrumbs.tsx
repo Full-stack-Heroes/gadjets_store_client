cb622f6ad3d99d0f7b4a0f77ea8b09a1588c032c
Denys Didenko <ddidenko441@gmail.com>
2023-08-09 20:34:20 +0300

    fix breadcrumbs logic

--- src/components/BreadCrumbs/BreadCrumbs.tsx
+++ src/components/BreadCrumbs/BreadCrumbs.tsx
@@ -4,43 +4,34 @@ import { Link, useLocation } from 'react-router-dom';
 import home from '../../assets/icons/Home.svg';
 import { Arrow } from '../Arrow/Arrow';
 import classNames from 'classnames/bind';
-// import { capitalizeWord, formatProductName } from '../../utils/helpers';
+import { capitalizeWord, formatProductName } from '../../utils/helpers';
 
 const cn = classNames.bind(styles);
 
 export const BreadCrumbs: FC = () => {
   const location = useLocation();
-
-  // breadcrumbs[0] = capitalizeWord(breadcrumbs[0]);
-
-  // if (breadcrumbs.length === 2) {
-  //   breadcrumbs[1] = formatProductName(breadcrumbs[1]);
-  // }
-
-  const asd = location.pathname.split('/').slice(1);
-
-  console.log(location, asd);
-
-  // useEffect(() => {
-  //   setBreadcrumbs(newLocation);
-  // }, [location]);
+  const breadcrumbs = location.pathname.split('/').slice(1);
+  breadcrumbs[0] = capitalizeWord(breadcrumbs[0]);
+  if (breadcrumbs.length === 2) {
+    breadcrumbs[1] = formatProductName(breadcrumbs[1]);
+  }
 
   return (
     <div className={cn('container', 'BreadCrumbs')}>
       <Link to="/">
         <img src={home} className={cn('home')}/>
       </Link>
-      {asd.map((breadcrumb, index) => (
+      {breadcrumbs.map((breadcrumb, index) => (
         <span key={breadcrumb}>
           <span className={cn('arrow')}>
             <Arrow />
           </span>
-          {index === asd.length - 1
+          {index === breadcrumbs.length - 1
             ? (
               <span className={cn('breadcrumb')}>{breadcrumb}</span>
             ): (
               <Link
-                to={`/${asd[index]}`}
+                to={`/${breadcrumbs[index]}`}
                 className={cn('breadcrumb', 'breadcrumb--active')}
               >
                 {breadcrumb}
